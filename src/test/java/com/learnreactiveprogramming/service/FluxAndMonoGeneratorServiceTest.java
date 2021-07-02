a10410d6e295b795a40f847dbd7f4ea11d5ac97f
kmakrutin <kirill.makrutin@touchnet.com>
2021-07-02 18:36:52 +0300

    FluxAndMonoGeneratorService step verifier test

--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 public class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    public void namesFlux() {
+        // given
+
+        // when
+        final var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        // then
+        StepVerifier.create(namesFlux)
+                .expectNext("alex", "ben", "chloe")
+                .verifyComplete();
+    }
 }
