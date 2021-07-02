775fe6e9b61636ecd64997ad7bbe409de3c72da6
kmakrutin <kirill.makrutin@touchnet.com>
2021-07-02 14:54:50 +0300

    Imperative vs Functional

--- src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,4 +1,23 @@
 package com.learnreactiveprogramming.functional;
 
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.toList;
+
 public class FunctionalExample {
+    public static void main(String[] args) {
+        final var filteredNames = List.of("alex", "ben", "chloe", "adam", "adam")
+                .stream()
+                .filter(greaterThan(3))
+                .distinct()
+                .map(String::toUpperCase)
+                .sorted()
+                .collect(toList());
+        System.out.println(filteredNames);
+    }
+
+    private static Predicate<? super String> greaterThan(int size) {
+        return name -> name.length() > size;
+    }
 }
--- src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,4 +1,22 @@
 package com.learnreactiveprogramming.imperative;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImperativeExample {
+    public static void main(String[] args) {
+        final var names = List.of("alex", "ben", "chloe", "adam", "adam");
+        final var filteredNames = namesGreaterThanSize(names, 3);
+        System.out.println(filteredNames);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> names, int size) {
+        var filteredNames = new ArrayList<String>();
+        for (String name : names) {
+            if (name.length() > size && !filteredNames.contains(name))
+                filteredNames.add(name);
+        }
+
+        return filteredNames;
+    }
 }
