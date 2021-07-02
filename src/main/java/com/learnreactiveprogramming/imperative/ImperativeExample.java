410e61605c491cbe9cec3427ec5c5fc6090474d7
kmakrutin <kirill.makrutin@touchnet.com>
2021-07-02 14:56:28 +0300

    Imperative vs Functional

--- src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,12 +1,14 @@
 package com.learnreactiveprogramming.imperative;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ImperativeExample {
     public static void main(String[] args) {
         final var names = List.of("alex", "ben", "chloe", "adam", "adam");
         final var filteredNames = namesGreaterThanSize(names, 3);
+        Collections.sort(filteredNames);
         System.out.println(filteredNames);
     }
 
@@ -14,7 +16,7 @@ public class ImperativeExample {
         var filteredNames = new ArrayList<String>();
         for (String name : names) {
             if (name.length() > size && !filteredNames.contains(name))
-                filteredNames.add(name);
+                filteredNames.add(name.toUpperCase());
         }
 
         return filteredNames;
