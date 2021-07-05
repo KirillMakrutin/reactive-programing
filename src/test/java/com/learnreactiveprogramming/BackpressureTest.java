9f268266078456bcb698706cde836a16b88e12fb
kmakrutin <kirill.makrutin@touchnet.com>
2021-07-05 17:48:19 +0300

    Base back pressure

--- src/test/java/com/learnreactiveprogramming/BackpressureTest.java
+++ src/test/java/com/learnreactiveprogramming/BackpressureTest.java
@@ -1,4 +1,87 @@
 package com.learnreactiveprogramming;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.SignalType;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static reactor.core.Exceptions.propagate;
+
+@Slf4j
 public class BackpressureTest {
+
+    @Test
+    public void testBackPressure() {
+
+        final var rangeFlux = Flux.range(1, 100).log();
+
+//        rangeFlux.subscribe(next -> log.info("Number is {}", next));
+        rangeFlux.subscribe(new BaseSubscriber<>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(2);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                log.info("Number is {}", value);
+                if (value > 50) {
+                    cancel();
+                } else {
+                    request(2);
+                }
+            }
+
+            @Override
+            protected void hookOnCancel() {
+                log.info("Hook on cancel");
+            }
+        });
+    }
+
+    @Test
+    public void testBackPressure_2() {
+
+        final var rangeFlux = Flux.range(1, 100).log().subscribeOn(Schedulers.boundedElastic());
+
+        AtomicBoolean continueRead = new AtomicBoolean(true);
+        BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(2);
+
+        rangeFlux.subscribe(new BaseSubscriber<>() {
+            @Override
+            protected void hookOnNext(Integer value) {
+                try {
+                    blockingQueue.put(value);
+                } catch (InterruptedException e) {
+                    throw propagate(e);
+                }
+            }
+
+            @Override
+            protected void hookFinally(SignalType type) {
+                stop();
+            }
+
+            private void stop() {
+                continueRead.set(false);
+            }
+        });
+
+        while (continueRead.get()) {
+            try {
+                log.info("Next number is: {}", blockingQueue.take());
+                MILLISECONDS.sleep(200);
+            } catch (InterruptedException e) {
+                throw propagate(e);
+            }
+        }
+    }
 }
